e33df4015e3ff663200c5f009fdadafd432a013d
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-17 07:07:41 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -10,7 +10,8 @@ def third_challenge
         :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => ""
+        :age => "50"
         },
       :hero_friends => []
    }, 
