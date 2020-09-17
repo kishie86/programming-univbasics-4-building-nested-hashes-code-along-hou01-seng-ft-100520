1b4a6a269374d7ee048a010e5ad1f5852270043a
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-17 07:21:12 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,22 +2,22 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-      :name => "Lord Montague"  
-      :age => "53"
+      name: "Lord Montague",  
+      age: "53"
         },
       :matriarch => {
-        :name => "Lady Montague"
-        :age => "54"
+        name: "Lady Montague",
+        age: => "54"
         },
       :hero => {
-        :name => "Romeo"
-        :age => "15"
-        :status => "alive"
+        name: "Romeo",
+        age: "15",
+        status: "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
+      patriarch: {
         :name => "Lord Capulet"
         :age => "50"
         },
